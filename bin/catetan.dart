bb2854292f8be7b92536063a6eb86f948885e5a2
unknown <abdullahalfaiq500@gmail.com>
2024-09-26 10:39:33 +0700

    first commit

---
+++ bin/catetan.dart
@@ -0,0 +1,17 @@
+void constAndFriends() {
+  late String kelas; //DEKLARASI => PROSES PEMBUATAN VARIABLE
+  kelas = "9"; //INISIALISASI => MEMBERIKAN NILAI VARIABLE
+  kelas = "10";
+
+  final String bahan; //DEKLARASI
+  bahan = "kayu"; //INISIALISASI
+  // bahan = "besi";
+
+  const String nama = "faiq"; //DEKLARASI DAN INISIALISASI
+  // nama = "dudul";
+
+  var umur = 15;
+  print(umur);
+  umur = 25;
+  print(umur);
+}
